143dd5596c2a4dac899416843fc67fe57549d505
Marc Parisi <phrocker@apache.org>
2021-04-06 15:33:52 -0400

    Update version for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='dwython',
-    version='0.1.3',
+    version='0.1.4',
     description='Datawave Python API',
+    ong_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/phrocker/dwython',
     author='Marc Parisi',
 
